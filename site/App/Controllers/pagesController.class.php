ea1ad700f53c747f42e2319704fc06c1e1689404
Eduardo Esteves <edluis97@hotmail.com>
2021-07-04 16:15:53 +0100

    New controller, model and view

--- site/App/Controllers/pagesController.class.php
+++ site/App/Controllers/pagesController.class.php
@@ -6,7 +6,16 @@ Class pagesController
 
   public function someMethod() {
     if (isset($_REQUEST['id'])) {
-      //Do whatever      
+     	//Calling getPageInfo method from pagesModel (site/App/Models/pagesModel.class.php)
+    	$pagesModel = new pagesModel();
+    	$pageInfo = $pagesModel->getPageInfo($_REQUEST['id']);
+
+    	view('pages/page', array(
+    		"info"	=> $pageInfo, //will the $info inside the view
+    	));
+    	//Calling the view (site/App/Views/pages/page.view.php)
+
+
     }
   }
 
--- site/App/Models/pagesModel.class.php
+++ site/App/Models/pagesModel.class.php
@@ -3,6 +3,12 @@
 Class pagesModel
 {
 
+	public function getPageInfo($pageID) {
+		$pageID = DB::escape_string($pageID);
+		//Assuming there is a pages table with an id field
+		$sql = "SELECT * FROM pages WHERE id = $pageID";
+		return DB::querySingleResult($sql);
+	}
 
 
 }
