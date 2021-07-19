b6533f9e62ff5f346a95cf837229bc8a43fb4305
Eduardo Esteves <edluis97@hotmail.com>
2021-07-19 21:40:43 +0100

    changed stuff

--- configs.php
+++ configs.php
@@ -1,27 +1,27 @@
 <?php 
 
 $siteInfo = array(
-  "root"  => $_SERVER['DOCUMENT_ROOT'].'/undressed-mvc',
-  "baseURL" => '/undressed-mvc/',
+  "root"  => $_SERVER['DOCUMENT_ROOT'],
+  "baseURL" => '/',
 );
 
 $db = array(
-  'host'  => 'localhost',
-  'user'  => 'root',
-  'password'  => '',
-  'database'  => 'demo_store',
+  'host'  => 'DB_HOST',
+  'user'  => 'DB_USER',
+  'password'  => 'DB_PASS',
+  'database'  => 'DB_NAME',
 );
 
 $smtp = array(
-  'sender_address' => '',
-  'sender_alias'  => '',
-  'password'  => '',
-  'port'  => '',
-  'secure'  => '',
-  'auth'  => '',
-  'debug' => 0, //PHPMailer Debug 0 to 4
-  'smtp_server' => '',
-  'to_address'  => '',
+  'smtp_server' => 'SMTP_SERVER',
+  'port'  => 'SMTP_PORT',
+  'sender_address' => 'SMTP_ADDRESS',
+  'sender_alias'  => 'SMTP_ALIAS',
+  'password'  => 'SMTP_PASS',  
+  'secure'  => 'ssl', //ssl or tsl
+  'auth'  => true,
+  'debug' => 0, //PHPMailer Debug 0 to 4  
+  'to_address'  => 'DEFAULT_EMAIL_DESTINATION',
   'to_alias'  => '',
   'reply_address' => '',
   'reply_alias' => '',
